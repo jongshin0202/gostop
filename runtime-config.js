aa493424c65991c599f227bd5de42eb4fe155d99
Jong-Wook Shin <jwshin1@gmail.com>
2026-09-24 21:26:39 -0500

    Keep runtime config free of async fullscreen loader

--- runtime-config.js
+++ runtime-config.js
@@ -1,3 +1,2 @@
 // Generated public configuration; this URL is not a secret.
 globalThis.GOSTOP_CONFIG=Object.freeze({"serverUrl":""});
-if(typeof document!=='undefined'){const s=document.createElement('script');s.src='mobile-fullscreen.js?v=20260924-4';s.async=false;document.head.appendChild(s);}
